011e8c5ec6097dca4be7c85f1ee13b9607da2bf6
Jake O'Shannessy <joshannessy@gmail.com>
2019-09-23 10:47:26 +0100

    Add convenience function.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod decode;
 mod spec;
 pub use decode::{decode_fds_file, FDSFile};
 use namelist::NamelistFile;
+use std::io::Read;
+use std::path::Path;
 
 pub fn parse_fds_input(i: &[u8]) -> NamelistFile {
     let namelist_spec = spec::init_spec();
@@ -12,5 +14,13 @@ pub fn parse_fds_input(i: &[u8]) -> NamelistFile {
 
 pub fn parse_and_decode_fds_input(i: &[u8]) -> FDSFile {
     let namelist_file = parse_fds_input(i);
+    decode_fds_file(&namelist_file)
+}
+
+pub fn parse_and_decode_fds_input_file(path: &Path) -> FDSFile {
+    let mut f = std::fs::File::open(path).unwrap();
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    let namelist_file = parse_fds_input(&buf);
     decode_fds_file(&namelist_file)
 }
